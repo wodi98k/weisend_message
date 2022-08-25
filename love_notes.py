78445321d5b9192451ece441a3ceaddaab5dd7bc
wodi98k <107985812+wodi98k@users.noreply.github.com>
2022-08-25 22:21:18 +0800

    Update love_notes.py

--- love_notes.py
+++ love_notes.py
@@ -1,20 +1,24 @@
-from operator import ge
 import requests
 import json
+import random
 headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.63"
     }
 def love_notes():
-    
-    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
-    r = requests.get(url=url,headers=headers)
-    data = json.loads(r.text)
-    return data['returnObj'][0]
+    notes = [
+        "大猪起床啦","天天当懒狗,程序都比你积极啦",
+        '起床！起床！起床',
+        '爱你爱你,希望今天不会吵架',
+        '地球爆炸啦,快跑啦',
+        '今天不努力,明天变垃圾',
+        '我在深圳很想你',
+        '晚起毁上午，早起傻一天，所以还是不起为妙。',
+        '不起床的猪'
+    ]
+    return random.choice(notes)
 def aiciba():
     url = "http://open.iciba.com/dsapi/"
- 
     r = requests.get(url,headers=headers)
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch + note_en
-
