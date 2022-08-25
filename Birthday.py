b1ae2373bdb8d91c93f82fff82f6613d2d019480
wodi98k <107985812+wodi98k@users.noreply.github.com>
2022-08-25 22:20:39 +0800

    Update Birthday.py

--- Birthday.py
+++ Birthday.py
@@ -1,9 +1,17 @@
 import datetime
-from hashlib import new
 def love_day():
     in_date = '2021-5-28'
     dt = datetime.datetime.strptime(in_date, "%Y-%m-%d")
     out_date = (datetime.datetime.now()-dt)
+    new_data = out_date.days
+    new_data=str(new_data)
+    return new_data+"天"
+def birthday():
+    in_date = "-11-12"
+    dt = datetime.datetime.now().strftime("%Y")
+    new_date = dt+in_date
+    new_data = datetime.datetime.strptime(new_date, "%Y-%m-%d")
+    out_date = new_data-datetime.datetime.now()
     new_data = out_date.days
     new_data=str(new_data)
     return new_data+"天"
