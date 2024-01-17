b5c410af12c35aaa99916cf0e6798edbf7455e94
wodi98k <107985812+wodi98k@users.noreply.github.com>
2024-01-17 21:56:17 +0800

    Update login_nssctf.py

--- login_nssctf.py
+++ login_nssctf.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
     chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
     chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
-    # chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+    chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
     driver = webdriver.Chrome(chrome_options=chrome_options)
